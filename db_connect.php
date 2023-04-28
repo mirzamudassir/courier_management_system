d4a7b8da663fe113a9bc8ede8283f022449cc3be
Mudassir Mirza <mudassir@assetize.today>
2023-04-29 01:32:29 +0500

    initial commit

---
+++ db_connect.php
@@ -0,0 +1,4 @@
+<?php 
+
+// $conn= new mysqli('localhost','id20673184_root','jCx6k#ulwB^PLQ<b','id20673184_cms_db')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','root','','cms_db')or die("Could not connect to mysql".mysqli_error($con));
